82b2979ec89e09d8da82205b04d1c1d926ed6361
Agra-H <agrahardianto11@gmail.com>
2023-03-27 11:58:13 +0700

    second commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class  Contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Perkenalkan </h1>";
+        echo "nama Saya Agra Hardianto
+              Saya tinggal di Daerah Bekasi
+              olah raga yang saya Sukai adalah 
+              game";
+
+    }
+}
